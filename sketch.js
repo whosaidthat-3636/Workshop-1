c9361841cd21829678a84c840c43dafa5ce03a97
whosaidthat-3636 <jong788@aucklanduni.ac.nz>
2025-01-19 11:47:32 +0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,17 +1,19 @@
 function setup() {
   createCanvas(550, 400);
-  background(98,162,107);
- 
-  for (x = 0; x <= 15; x++) {
+  background(98, 162, 107);
+
+  for (let x = 0; x <= 15; x++) {
     ball(random(0, width), random(0, height), 30, color(random(255), random(255), random(255)));
   }
 }
 
-function draw() {
+function draw() {}
+
+function ball(x, y, size, baseColor) {
+  fill(255); 
+  noStroke();
+  ellipse(x, y, size, size);
+
+  fill(baseColor);
+  ellipse(x, y, size * 1.015, size * 0.6); 
 }
-
-function ball(x, y , size, color) {
-  fill(random(255), random(255), random(255));
-noStroke(); 
-  ellipse(x, y, size, size);}
-
